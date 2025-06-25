83c539335de5691dab4ba8120faee981f077b25c
ald0111 <114394233+ald0111@users.noreply.github.com>
2025-06-25 14:29:56 +0530

    selection sort

---
+++ cpp/selectionSort.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+void selection_sort(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        int min = i;
+        for (int j = i + 1; j < n; j++)
+        {
+            if (arr[j] < arr[min])
+            {
+                min = j;
+            }
+        }
+        int temp = arr[i];
+        arr[i] = arr[min];
+        arr[min] = temp;
+    }
+    return;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+    selection_sort(arr, n);
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+}
